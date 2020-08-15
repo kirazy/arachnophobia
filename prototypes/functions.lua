0663b5768c885e0a7c5f2cf0d340da18886670e4
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-08-14 17:19:57 -0700

    Add laser spots where the feet go

--- prototypes/functions.lua
+++ prototypes/functions.lua
@@ -26,9 +26,55 @@ for n = 1, 8 do
         spider_leg.graphics_set[field] = nil
     end
 
+    -- Optionally display laser feet
+    if settings.startup["arachnophobia-display-laser-feet"].value == true then
+        spider_leg.graphics_set.lower_part = {
+            bottom_end = {
+                layers = {
+                    {
+                        filename = "__arachnophobia__/graphics/spidertron-legs-lower-tip.png",
+                        width = 18,
+                        height = 56,
+                        shift = util.by_pixel(0, -21),
+                        hr_version = {
+                            filename = "__arachnophobia__/graphics/hr-spidertron-legs-lower-tip.png",
+                            width = 34,
+                            height = 112,
+                            shift = util.by_pixel(0, -21),
+                        }
+                    },
+                    -- Illuminate the laser light, pending support in base game
+                    -- {
+                    --     filename = "__arachnophobia__/graphics/spidertron-legs-lower-tip-light.png",
+                    --     width = 18,
+                    --     height = 56,
+                    --     shift = util.by_pixel(0, -21),
+                    --     draw_as_sprite = false,
+                    --     draw_as_light = true,
+                    --     hr_version = {
+                    --         filename = "__arachnophobia__/graphics/hr-spidertron-legs-lower-tip-light.png",
+                    --         width = 34,
+                    --         height = 112,
+                    --         shift = util.by_pixel(0, -21),
+                    --         draw_as_sprite = false,
+                    --         draw_as_light = true,
+                    --     }
+                    -- }
+                }
+            },
+            bottom_end_length = 1,
+            middle = util.empty_sprite(),
+            middle_offset_from_bottom = 0.65,
+            middle_offset_from_top = 0.45,
+            top_end = util.empty_sprite(),
+            top_end_length = 1
+        }
+    else
+        -- Hide footstep particles and sounds
+        spidertron.spider_engine.legs[n].leg_hit_the_ground_trigger = nil
+        
+    end
+
     -- Silence the legs
     spider_leg.working_sound = nil
-
-    -- Hide footstep particles
-    spidertron.spider_engine.legs[n].leg_hit_the_ground_trigger = nil
 end
---
+++ settings.lua
@@ -0,0 +1,14 @@
+-- Copyright (c) 2020 Kirazy
+-- Part of Arachnophobia
+--     
+-- See LICENSE.md in the project directory for license information.
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "arachnophobia-display-laser-feet",
+        setting_type = "startup",
+        order = "a",
+        default_value = true,
+    }
+})
